e3ac196f2fe007a488c365e23bd66f448c27c090
jcalao44 <54120051+jcalao44@users.noreply.github.com>
2020-03-15 19:39:29 -0300

    Implementação de girar a direita e a esquerda

--- oo/carro.py
+++ oo/carro.py
@@ -83,17 +83,36 @@ A Direção terá a responsabilidade de controlar a direção. Ela oferece os se
         >>> carro.calcular_velocidade
         0
         >>> carro.calcular_direcao()
-        >>> 'Norte'
+        'Norte'
         >>> carro.girar_a_direita()
         >>> carro.calcular_direcao()
-        >>> 'Leste'
+        'Leste'
         >>> carro.girar_a_esquerda()
         >>> carro.calcular_direcao()
-        >>> 'Norte'
+        'Norte'
         >>> carro.girar_a_esquerda()
         >>> carro.calcular_direcao()
-        >>> 'Oeste'
+        'Oeste'
 """
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        return self.motor.acelerar()
+
+    def frear(self):
+        return self.motor.frear()
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.girar_a_esquerda()
 
 
 NORTE = 'Norte'
